42b007e0629eeb929dc51d9f060f58420e7d3ad3
Omar Gamal <omar.gamal201@outlook.sa>
2026-01-21 19:41:49 +0200

    Editing JWT & Auth

--- Back-End/utils/jwt.js
+++ Back-End/utils/jwt.js
@@ -1,6 +1,14 @@
+const jwt = require("jsonwebtoken");
+
 // create JWT
 function generateToken(user) {
-  return jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE || '7d',
-  });
+  return jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_EXPIRE || "7d",
+    }
+  );
 }
+
+module.exports = generateToken;
